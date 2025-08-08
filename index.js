ef804f1eb4e2d0490bb439611b9d4f4ad8305351
Joseph Rodelo Suarez <rodelosuaresj@gmail.com>
2025-08-07 23:08:34 -0500

    feat(app): integra clase de búsquedas y estructura inicial para mostrar clima
    - Se importó e instanció la nueva clase `Busquedas` desde `models/busquedas.js`.
    - Se preparó el flujo para la búsqueda de ciudades y futura integración de clima:
      - Se solicita al usuario el nombre de una ciudad.
      - Se muestran datos simulados del clima y geolocalización de ejemplo.
    - Se mantiene el flujo con `inquirerMenu`, `inputText`, `confirmar`, y `pausa`.

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 require("colors");
 const axios = require('axios').defaults;
 
-const { inquirerMenu, pausa, confirmar, inputText} = require("./helpers/inquirer.js")
+const { inquirerMenu, pausa, confirmar, inputText} = require("./helpers/inquirer.js");
+const Busquedas = require("./models/busquedas.js");
 
 const main = async () => {
 
+    const busquedas = new Busquedas();
     let opt= 0;
     do{
         opt = await inquirerMenu();
         switch(opt){
             case 1:
-                const response = await inputText("Ciudad:");
-                console.log(response);
+                // Mostrar mensaje
+                const lugar = await inputText("Ciudad:");
+                console.log(lugar);
+
+                // Buscar los lugares
+
+                // Seleccionar el lugar
+
+                // Datos del clima
+
+                //motrar resultados
+                console.log('\nInformacion del lugar\n'.green);
+                console.log('Ciudad:', 'San Jose');
+                console.log('Lat:', 9.9281);
+                console.log('Lng:', -84.0907);
+                console.log('Temperatura:', 25);    
+                console.log('Minima:', 20);
+                console.log('Maxima:', 30);
+
                 break;
             case 2:
                 console.log();
