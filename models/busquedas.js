f58b4ed7c4dd35d21d19112e16dd19ea3637bb3e
Joseph Rodelo Suarez <rodelosuaresj@gmail.com>
2025-08-07 23:09:16 -0500

    feat(busquedas): crea clase base para manejar historial y búsqueda de ciudades
    - Se creó la clase `Busquedas` con una propiedad `historial` que contiene ciudades por defecto.
    - Se agregó el método `ciudad(lugar)` para simular búsqueda de ciudades (actualmente imprime mensaje y retorna array vacío).
    - Se dejó preparado el constructor para lectura de base de datos futura (`TODO`).
    
    BREAKING CHANGE: nueva funcionalidad estructural que será base para la lógica de búsquedas por ciudad.

---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+class Busquedas {
+
+    historial = [ 'Madrid', 'paris', 'san jose', 'cartagena', ];
+
+    constructor() {
+        // TODO: leer la db si existe
+    }
+
+    async ciudad ( lugar  = '') {
+        console.log(`Buscando lugar: ${lugar}...`);
+        return [];
+    }
+
+}
+
+
+
+
+module.exports = Busquedas;
